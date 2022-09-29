2b310ec8dc9f848d304bb767094627891801776a
solomonugo <kingsolodawarrior@gmail.com>
2022-09-29 17:20:22 +0100

    different device size

---
+++ static/js/main.js
@@ -0,0 +1,28 @@
+
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:3.5,
+            nav:true
+        },
+        600:{
+            items:4,
+            nav:false
+        },
+        1000:{
+            items:6,
+            nav:true,
+            loop:true
+        }
+    }
+})
+
+function toggle() {
+   
+    const toggle = document.getElementById('search-click')
+   
+    
+}

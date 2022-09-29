41e65da81a5efae6b5e7eebfeb2b76c5f3730288
solomonugo <kingsolodawarrior@gmail.com>
2022-09-29 21:45:32 +0100

    mobile store view

--- static/js/main.js
+++ static/js/main.js
@@ -1,21 +1,19 @@
 
 $('.owl-carousel').owlCarousel({
     loop:true,
-    margin:10,
+    autoplay: true,
+    autoplayTimeout: 1000,
+    margin:5,
     responsiveClass:true,
     responsive:{
         0:{
-            items:3.5,
-            nav:true
+            items:2,
         },
         600:{
             items:4,
-            nav:false
         },
         1000:{
-            items:6,
-            nav:true,
-            loop:true
+            items:4.5,
         }
     }
 })

cd5c297e78ddc271ae5fed072871d332ec736600
solomonugo <kingsolodawarrior@gmail.com>
2022-10-05 20:58:16 +0100

    cart function

--- store/views.py
+++ store/views.py
@@ -11,7 +11,14 @@ def store(request):
 
 #Cart function
 def cart(request):
-    context = {}
+
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        items = order.orderitem_set.all()
+    else:
+        items = []
+    context = {'items': items}
     return render(request, 'store/cart.html', context)
 
 

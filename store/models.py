9e6b34693cfb978fe3565e1be28793b1d6668d00
Solomon Bestz <Kingsolodawarrior@gmail.com>
2022-10-13 13:39:03 +0100

    django signals

--- store/models.py
+++ store/models.py
@@ -19,7 +19,8 @@ class Customer(models.Model):
     email = models.CharField(max_length=200, null=True)
 
     def __str__(self):
-        return self.name
+        return str(self.user)
+
 
 class Product(models.Model):
     name = models.CharField(max_length=200)  
--- store/signals.py
+++ store/signals.py
@@ -1,12 +1,20 @@
 from django.db.models.signals import post_save
-from account.models import NewUser
 from django.dispatch import receiver
+
+from account.models import NewUser
 from .models import Customer
 
 @receiver(post_save, sender=NewUser)
 def create_customer(sender, instance, created, **kwargs):
-    print("I created New User")
+    if created:
+        Customer.objects.create(user=instance, name=instance.middle_name, email=instance.email)
+        print("Customer Created")
+
 
 @receiver(post_save, sender=NewUser)
-def save_customer(sender, instance, **kwargs):
-    print("customer saved")
+def update_customer(sender, instance, created, **kwargs):
+    if created == False:
+        instance.customer.save()
+        print('Customer Updated')
+
+

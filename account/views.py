199a95422b4ec987ce7d42963141c4acb242efe6
Solomon Bestz <Kingsolodawarrior@gmail.com>
2022-10-14 22:16:17 +0100

    account view function

--- account/views.py
+++ account/views.py
@@ -10,6 +10,31 @@ app_name = "account"
 auth = BackEndSetting()
 
 def authentication(request):
+    if request.method == "POST":
+        first_name = request.POST.get("")
+        last_name = request.POST.get("")
+        middle_name = request.POST.get("")
+        email = request.POST.get("")
+        password1 = request.POST.get("")
+        password2 = request.POST.get("")
+    
+    elif request.method == "POST":
+        email = request.POST.get("")
+        password = request.POST.get("")
+
+        user = auth.authenticate(request, email=email, password=password)
+        if user is not None and user.is_staff == False:
+            login(request, user)
+            if user.is_active == True:
+                messages.success(request, "Successfully logged In.")
+                return redirect("store")
+            else:
+                return redirect("404")
+        else:
+            messages.error(request, "Incorrect Login Details")
+            return redirect('signin')
+        
+
     return render(request, "account/authentication.html")
 
 def signout(request):

191042f49e6d2e6d8d351381ada8f97dd29dd947
Solomon Bestz <Kingsolodawarrior@gmail.com>
2022-11-01 13:38:38 +0100

    dashboard view integration

--- vinchadmin/urls.py
+++ vinchadmin/urls.py
@@ -2,7 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('dashboard/', views.home, name='adminhome'),
-    
-    
+    path('dashboard/', views.dashboard, name='dashboard' ),
 ]
--- vinchadmin/views.py
+++ vinchadmin/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
 
-# Create your views here.
-def home(request):
-    return render(request, 'admindashboard/home.html')
+
+def dashboard(request):
+    return render(request, 'admindashboard/dashboard.html')
